f954dd6b5c4c9a46920e15e907e28bdc493a3de6
Fanny <fanny_ve@hotmail.com>
2019-10-24 22:18:49 +0200

    Dateikopf verändert

--- Bruche/Bruche/Controller/main.cs
+++ Bruche/Bruche/Controller/main.cs
@@ -1,13 +1,5 @@
 ﻿/* Autorin: 	 	Fanny Vera
  * Datum: 	 	 	18.09.19
- * Beschreibung: 	Hauptfunktion
- * Veränderungen: 
- * 18.09.19  		Entwicklungsbeginn
- * 24.09.19  		Class Bruch angefertigt
- * 25.09.19 		MVC Modelle
- * 01.10.19  		Weiter bearbeitet
- * 02.10.19         Zuweisung integriert
- * 08.10.19         vorl. Fertig
 */
 
 using System;
